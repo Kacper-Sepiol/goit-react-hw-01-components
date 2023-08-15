9203c5c0563f3b07359132af542e05847927e9dc
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-08-15 12:37:35 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,21 @@
 import Profile from './profile/Profile';
 import user from '../components/user.json';
+import Statistics from './statistics/Statistics';
+import data from './data.json';
 
 const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    ></Profile>
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      ></Profile>
+
+      <Statistics title="Upload stats" stats={data} />
+    </div>
   );
 };
 
--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,24 +1,22 @@
-const Statistics = ({ title, stats, color }) => (
-  <section class="statistics">
-    <h2 class="title">Upload stats</h2>
+import data from '../data.json';
 
-    <ul class="stat-list">
-      <li class="item">
-        <span class="label">.docx</span>
-        <span class="percentage">4%</span>
-      </li>
-      <li class="item">
-        <span class="label">.mp3</span>
-        <span class="percentage">14%</span>
-      </li>
-      <li class="item">
-        <span class="label">.pdf</span>
-        <span class="percentage">41%</span>
-      </li>
-      <li class="item">
-        <span class="label">.mp4</span>
-        <span class="percentage">12%</span>
-      </li>
-    </ul>
-  </section>
-);
+const Statistics = ({ title, stats, color }) => {
+  return (
+    <section className="statistics">
+      <h2 className="title">Upload stats</h2>
+
+      <ul className="stat-list">
+        {stats.map(stat => (
+          <div>
+            <li className="item" key={stat.id}>
+              <span className="label">{stat.label}</span>
+              <span className="percentage">{stat.percentage}%</span>
+            </li>
+          </div>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Statistics;
