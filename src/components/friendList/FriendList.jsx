aba41f80882838ceefaf695e5990bf970abdae1e
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-08-15 13:04:52 +0200

    update

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,8 +1,11 @@
 import friendss from './friends.json';
 
 const FriendList = ({ friends }) => (
-<ul class="friend-list">
-    {friends.map(friend => ())}
-</ul>);
+  <ul class="friend-list">
+    {friends.map(friend => (
+      <li></li>
+    ))}
+  </ul>
+);
 
 export default FriendList;
