b14a83f117f2324805e6e72aa92cb5c3a4af6e30
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-08-15 14:19:58 +0200

    update

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,27 +1,33 @@
-import profile from './profile.module.css';
-import avatarIcon from './avatar.module.css';
+import profileCss from './profile.module.css';
+import avatarCss from './avatar.module.css';
+import nameCss from './name.module.css';
+import tagCss from './tag.module.css';
+import locationCss from './location.module.css';
+import statsCss from './stats.module.css';
+import labelCss from './label.module.css';
+import quantityCss from './quantity.module.css';
 
 const Profile = ({ username, tag, location, avatar, stats }) => (
-  <div className={profile.profile}>
+  <div className={profileCss.profile}>
     <div className="description">
-      <img src={avatar} alt="User avatar" className={avatarIcon.avatar} />
-      <p className="name">{username}</p>
-      <p className="tag">{tag}</p>
-      <p className="location">{location}</p>
+      <img src={avatar} alt="User avatar" className={avatarCss.avatar} />
+      <p className={nameCss.name}>{username}</p>
+      <p className={tagCss.tag}>{tag}</p>
+      <p className={locationCss.location}>{location}</p>
     </div>
 
-    <ul className="stats">
+    <ul className={statsCss.stats}>
       <li>
-        <span className="label">Followers</span>
-        <span className="quantity">{stats.followers}</span>
+        <span className={labelCss.label}>Followers:</span>
+        <span className={quantityCss.quantity}>{stats.followers}</span>
       </li>
       <li>
-        <span className="label">Views</span>
-        <span className="quantity">{stats.views}</span>
+        <span className={labelCss.label}>Views:</span>
+        <span className={quantityCss.quantity}>{stats.views}</span>
       </li>
       <li>
-        <span className="label">Likes</span>
-        <span className="quantity">{stats.likes}</span>
+        <span className={labelCss.label}>Likes:</span>
+        <span className={quantityCss.quantity}>{stats.likes}</span>
       </li>
     </ul>
   </div>
