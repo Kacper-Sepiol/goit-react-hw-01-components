c7e328290e7ea2b8558039c4151a0b2dc7a614b0
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-08-15 13:59:11 +0200

    update

--- src/components/transactionHistory/TransactionHistory.jsx
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -2,16 +2,15 @@ import transactions from '../transactions.json';
 
 const TransactionHistory = ({ items }) => (
   <table class="transaction-history">
+    <thead>
+      <tr>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Currency</th>
+      </tr>
+    </thead>
     {transactions.map(transaction => (
       <div>
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
-          </tr>
-        </thead>
-
         <tbody>
           <tr key={transaction.id}>
             <td>{transaction.type}</td>
