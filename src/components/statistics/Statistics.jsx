c908ab4c042df5f4feece95fbfa0536af22ed597
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-08-15 15:52:48 +0200

    update

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,9 +1,9 @@
 import data from '../data.json';
 
-const Statistics = ({ title, stats, color }) => {
+const Statistics = ({ title, stats }) => {
   return (
     <section className="statistics">
-      <h2 className="title">Upload stats</h2>
+      <h2 className="title">{title}</h2>
 
       <ul className="stat-list">
         {stats.map(stat => (
