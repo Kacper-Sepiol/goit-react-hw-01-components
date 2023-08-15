d5cae5154bffdd0b860472844f80fb91374a09b3
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-08-15 13:11:19 +0200

    update

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,9 +1,21 @@
-import friendss from './friends.json';
+import friends from '../friends.json';
+
+const FriendListItem = ({ avatar, name, isOnline }) => (
+  <li className="item">
+    <span className="status">{isOnline}</span>
+    <img className="avatar" src={avatar} alt="User avatar" width="48" />
+    <p className="name">{name}</p>
+  </li>
+);
 
 const FriendList = ({ friends }) => (
   <ul class="friend-list">
     {friends.map(friend => (
-      <li></li>
+      <FriendListItem
+        avatar={friend.avatar}
+        name={friend.name}
+        isOnline={friend.isOnline}
+      ></FriendListItem>
     ))}
   </ul>
 );
