d8e08944b42a7f80e880962a8ef9aec9bf6bcd92
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-08-15 16:02:36 +0200

    update

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,16 +1,23 @@
 import data from '../data.json';
+import statisticCss from './statistics.module.css';
+import titleCss from './title.module.css';
+import statListCss from './statList.module.css';
+import labelCss from './label.module.css';
+import percentageCss from './percentage.module.css';
 
 const Statistics = ({ title, stats }) => {
   return (
-    <section className="statistics">
-      <h2 className="title">{title}</h2>
+    <section className={statisticCss.statistic}>
+      <h2 className={titleCss.title}>{title}</h2>
 
-      <ul className="stat-list">
+      <ul className={statListCss.statList}>
         {stats.map(stat => (
           <div>
             <li className="item" key={stat.id}>
-              <span className="label">{stat.label}</span>
-              <span className="percentage">{stat.percentage}%</span>
+              <span className={labelCss.label}>{stat.label}:</span>
+              <span className={percentageCss.percentage}>
+                {stat.percentage}%
+              </span>
             </li>
           </div>
         ))}
