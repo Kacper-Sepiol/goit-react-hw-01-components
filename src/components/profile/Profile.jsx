968be23b39c57350e3d950ab3fe7bdfc0bad8f9c
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-08-16 19:31:30 +0200

    update

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,33 +1,26 @@
 import profileCss from './profile.module.css';
-import avatarCss from './avatar.module.css';
-import nameCss from './name.module.css';
-import tagCss from './tag.module.css';
-import locationCss from './location.module.css';
-import statsCss from './stats.module.css';
-import labelCss from './label.module.css';
-import quantityCss from './quantity.module.css';
 
 const Profile = ({ username, tag, location, avatar, stats }) => (
   <div className={profileCss.profile}>
     <div className="description">
-      <img src={avatar} alt="User avatar" className={avatarCss.avatar} />
-      <p className={nameCss.name}>{username}</p>
-      <p className={tagCss.tag}>{tag}</p>
-      <p className={locationCss.location}>{location}</p>
+      <img src={avatar} alt="User avatar" className={profileCss.avatar} />
+      <p className={profileCss.name}>{username}</p>
+      <p className={profileCss.tag}>{tag}</p>
+      <p className={profileCss.location}>{location}</p>
     </div>
 
-    <ul className={statsCss.stats}>
+    <ul className={profileCss.stats}>
       <li>
-        <span className={labelCss.label}>Followers:</span>
-        <span className={quantityCss.quantity}>{stats.followers}</span>
+        <span className={profileCss.label}>Followers:</span>
+        <span className={profileCss.quantity}>{stats.followers}</span>
       </li>
       <li>
-        <span className={labelCss.label}>Views:</span>
-        <span className={quantityCss.quantity}>{stats.views}</span>
+        <span className={profileCss.label}>Views:</span>
+        <span className={profileCss.quantity}>{stats.views}</span>
       </li>
       <li>
-        <span className={labelCss.label}>Likes:</span>
-        <span className={quantityCss.quantity}>{stats.likes}</span>
+        <span className={profileCss.label}>Likes:</span>
+        <span className={profileCss.quantity}>{stats.likes}</span>
       </li>
     </ul>
   </div>
